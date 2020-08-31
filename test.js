8b63a787cf5b7bc4bebcb628a6c10e47602d0edf
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-08-31 13:03:34 +1200

    Update test.js

--- test.js
+++ test.js
@@ -190,6 +190,7 @@ function pet(){
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
+    this.sound.loop = true
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
