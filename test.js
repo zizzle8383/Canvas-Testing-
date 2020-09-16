34d1cdf78988ee204b0cecc213481ab087a87811
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-09-16 13:15:01 +1200

    Update test.js

--- test.js
+++ test.js
@@ -62,9 +62,13 @@ var myGameArea = {
          position.x = Math.round(position.x)
          position.y = Math.round(position.y)
           if (walking == 0){ 
+          if (position.y > 0 && position.y < 480){
+          if (position.x > 0 && position.x < 850){
             walking = 1
          int1 = setInterval(Loop1,1)
+           }
           }
+         }
      });
 
     },
