27be4592f6b48d75fdc2901271199754857491c4
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-09-16 12:23:51 +1200

    Update test.js

--- test.js
+++ test.js
@@ -168,6 +168,8 @@ function updateGameArea() {
      room =  room.replace(/["']/g, "");
      myBackground.image.src = "Rooms/"+room+"/bg.png"
      myForeground.image.src = "Rooms/"+room+"/FG.png"
+     try {clearInterval(int1)}
+     catch(err){console.log(err)}
   }
     
     
