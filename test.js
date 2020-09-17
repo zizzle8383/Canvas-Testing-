7af832da21216153a19d86a4e36368874dd2bb29
Alvarito050506 <donfrutosgomez@gmail.com>
2020-09-17 19:24:15 -0300

    Improvements and clean-up.

--- test.js
+++ test.js
@@ -1,82 +1,82 @@
 var myGamePiece;
 var myMsg;
-var a;
 var myMusic;
-var numba = 0
-var rooms = ["Original","Room2"]
-var room = "Original"
-var Ac = false;
+var rooms = ["original", "room2"];
+var roomID = 0;
+var room = "original";
 var petbounce = 2;
-var loop = true;
-var vx
-var vy
-var numb1 
-var int1
-var stoploop
-var currentroom
-var walking = 0
+var petLoop = true;
+var walking = false;
+var pet = "pet1";
+var vpos = {
+    "x": 0,
+    "y": 0
+};
 
-function start(){
-    document.getElementById("play").style.visibility = "hidden"
-    setTimeout(startGame,500)
+function start() {
+    document.getElementById("play").style.visibility = "hidden";
+    setTimeout(startGame, 500);
 }
+
 function startGame() {
-    document.getElementById("text").style.visibility = "visible"
-    document.getElementById("link").style.visibility = "visible"
-    document.getElementById("HatTest").style.visibility = "visible"
-    document.getElementById("button1").style.visibility = "visible"
-    document.getElementById("button2").style.visibility = "visible"
-    document.getElementById("button3").style.visibility = "visible"
+    document.getElementById("msg-text").style.visibility = "visible";
+    document.getElementById("pfp-link").style.visibility = "visible";
+    document.getElementById("hat-test").style.visibility = "visible";
+    document.getElementById("msg-send").style.visibility = "visible";
+    document.getElementById("pfp-set").style.visibility = "visible";
+    document.getElementById("hat-set").style.visibility = "visible";
     
-    myMusic = new sound("titleconcept.mp3");
+    myMusic = new sound("./titleconcept.mp3");
     myMusic.play();
-    myForeground = new component(850, 50, "Rooms/Original/FG.png", 0, 445, "image");
+    myForeground = new component(850, 50, `./rooms/${room}/fg.png`, 0, 445, "image");
     myMsg = new component("10px", "Courier New", "black", 280, 40, "text");
-    myGamePiece = new component(50, 50, "test12.png", 425, 240, "image");
-    myBackground = new component(850, 480, "Rooms/Original/bg.png", 0, 0, "image");
-    myNextArea =  new component(50, 50, "test12.png", 425, 240, "image");
-    myHat =  new component(30, 30, "", 425, 240, "image");
-    myPet = new component(30, 25, "pets/pet1.png", 425, 240, "image");
+    myGamePiece = new component(50, 50, "./player.png", 425, 240, "image");
+    myBackground = new component(850, 480, `rooms/${room}/bg.png`, 0, 0, "image");
+    myNextArea = new component(50, 50, "./player.png", 425, 240, "image");
+    myHat = new component(30, 30, "", 425, 240, "image");
+    myPet = new component(30, 25, `./pets/${pet}.png`, 425, 240, "image");
+    vpos.x = myGamePiece.x;
+    vpos.y = myGamePiece.y;
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+    "canvas": document.createElement("canvas"),
+    "start": function () {
         this.canvas.width = 850;
         this.canvas.height = 480;
         this.context = this.canvas.getContext("2d");
-        document.getElementById("canvas-container").insertBefore(this.canvas, null);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
-               window.addEventListener('keydown', function (e) {
-      myGameArea.keys = (myGameArea.keys || []);
-      myGameArea.keys[e.keyCode] = true;
-    });
-    window.addEventListener('keyup', function (e) {
-      myGameArea.keys[e.keyCode] = false;
-    });
-    
-      window.addEventListener("mousedown", function (e) {
-         currentroom = room
-         position = getMousePos(myGameArea.canvas, e);
-         position.x = Math.round(position.x)+10
-         position.y = Math.round(position.y)+5
-          if (walking == 0){ 
-          if (position.y > 0 && position.y < 480){
-          if (position.x > 0 && position.x < 850){
-            walking = 1
-         int1 = setInterval(Loop1,1)
-           }
-          }
-         }
-     });
+
+        document.getElementById("canvas-container").insertBefore(this.canvas, null);
+        window.addEventListener("keydown", function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        });
+        window.addEventListener("keyup", function (e) {
+            myGameArea.keys[e.keyCode] = false;
+        });
+        this.canvas.addEventListener("mousedown", function (e) {
+            currentroom = room;
+            vpos = getMousePos(myGameArea.canvas, e);
+            vpos.x -= 25;
+            vpos.y -= 45;
+            if (!walking) {
+                if (vpos.y > 0 && vpos.y < 480) {
+                    if (vpos.x > 0 && vpos.x < 850){
+                        walking = true;
+                        requestAnimationFrame(updateChar);
+                    }
+                }
+            }
+        });
 
     },
-    clear : function() {
+    "clear": function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    stop : function() {
+    "stop": function() {
         clearInterval(this.interval);
     }
 };
@@ -100,8 +100,8 @@ function component(width, height, color, x, y, type) {
                 this.x, 
                 this.y,
                 this.width, this.height);
-        } else if (type=="text") {
-             ctx.font = this.width + " " + this.height;
+        } else if (type == "text") {
+             ctx.font = `${this.width} ${this.height}`;
              ctx.fillStyle = color;
              ctx.fillText(this.text, this.x, this.y);
         } else {
@@ -116,114 +116,105 @@ function component(width, height, color, x, y, type) {
 }
 
 function updateGameArea() {
-    
     myGameArea.clear();
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
-    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -5; }
-    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 5; }
-    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -5; }
-    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 5; }
+
+    if (myGameArea.keys && myGameArea.keys[37]) {
+        myGamePiece.speedX = -5;
+    }
+    if (myGameArea.keys && myGameArea.keys[39]) {
+        myGamePiece.speedX = 5;
+    }
+    if (myGameArea.keys && myGameArea.keys[38]) {
+        myGamePiece.speedY = -5;
+    }
+    if (myGameArea.keys && myGameArea.keys[40]) {
+        myGamePiece.speedY = 5;
+    }
+
     myBackground.newPos();
     myBackground.update();
     myNextArea.update();
     myGamePiece.newPos();
     myGamePiece.update();
     myPet.x = myGamePiece.x - 30;
-    pet();
-    myPet.update();
+    updatePet();
     myHat.x = myGamePiece.x + 10;
     myHat.y = myGamePiece.y - 20;
     myHat.update();
     myForeground.newPos();
     myForeground.update();
-    if (Ac === true){
-    b = a.length;
- 
-   
-    
-    myMsg.text = a;
-    
-    myMsg.x = myGamePiece.x - b*5 + 24+b*2;  
-    myMsg.y = myGamePiece.y - 10;
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(myMsg.x, myMsg.y-10, ctx.measureText(myMsg.text).width, 15);
-    myMsg.update();
+
+    if (myMsg.text != undefined && myMsg.text != "") {
+        myMsg.x = myGamePiece.x - myMsg.text.length * 3 + 24;
+        myMsg.y = myGamePiece.y - 10;
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(myMsg.x, myMsg.y - 10, ctx.measureText(myMsg.text).width, 15);
+        myMsg.update();
     }
     
-  if (myGamePiece.x <= 30){
-     myGamePiece.x = 425
-      if (numba !== 0){
-      numba =  numba - 1
-      } else{
-          numba = 1
-      }
-     room = rooms[numba]
-     room =  room.replace(/["']/g, "");
-     myBackground.image.src = "Rooms/"+room+"/bg.png"
-     myForeground.image.src = "Rooms/"+room+"/FG.png"
-     
-     
-  }
+    if (myGamePiece.x <= 30) {
+        myGamePiece.x = 425
+        if (roomID != 0) {
+            roomID--;
+        } else {
+            roomID = 1;
+        }
+        room = rooms[roomID].replace(/['']/g, "");
+        myBackground.image.src = `./rooms/${room}/bg.png`;
+        myForeground.image.src = `./rooms/${room}/fg.png`;
+    }
     
-  if (myGamePiece.x >= 820){
-     myGamePiece.x = 425
-      if (numba !== 1){
-      numba =  numba + 1
-      } else{
-          numba = 0
-      }
-     room = rooms[numba]
-     room =  room.replace(/["']/g, "");
-     myBackground.image.src = "Rooms/"+room+"/bg.png"
-     myForeground.image.src = "Rooms/"+room+"/FG.png"
-     
-     
-  }
-    
-    
+    if (myGamePiece.x >= 820){
+        myGamePiece.x = 425;
+        if (roomID != 1) {
+            roomID++;
+        } else{
+            roomID = 0
+        }
+        room = rooms[roomID].replace(/['']/g, "");
+        myBackground.image.src = `./rooms/${room}/bg.png`;
+        myForeground.image.src = `./rooms/${room}/fg.png`;
+    }
 }
+
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-       x: evt.clientX - rect.left,
-       y: evt.clientY - rect.top
+       "x": evt.clientX - rect.left,
+       "y": evt.clientY - rect.top
     };
 }
 
-function send(){
-  Ac = true;
-  a = document.getElementById("text").value;
-  setTimeout(function stopsend(){ a=""}, 4000);
-  
-
-
-}
-function SetPFP(){
-   link = document.getElementById("link").value;
-  myGamePiece.image.src = link;
-  myGamePiece.update();
-  
+function send() {
+    myMsg.text = document.getElementById("msg-text").value;
+    setTimeout(function () {
+        myMsg.text = "";
+    }, 4000);
 }
 
-function Hat(){
-  myHat.image.src = "hats/"+document.getElementById("HatTest").value +".png";
+function setPFP() {
+    myGamePiece.image.src = document.getElementById("pfp-link").value;
+    myGamePiece.update();
 }
 
-function pet(){
-  if (petbounce <=20 && loop === true){
-   petbounce = petbounce + 1;
-  }else if(petbounce >= 1){
-    loop = false;
-    petbounce = petbounce - 1;
-  }else{
-    loop = true;
-  }
-  myPet.y = myGamePiece.y + 10 + petbounce;
-  
+function updateHat() {
+    myHat.image.src = `./hats/${document.getElementById("hat-test").value}.png`;
 }
 
-
+function updatePet() {
+    if (petbounce <= 20 && petLoop) {
+        petbounce++;
+    } else if (petbounce >= 1) {
+        petLoop = false;
+        petbounce--;
+    } else {
+        petLoop = true;
+    }
+    myPet.y = myGamePiece.y + 10 + petbounce;
+    myPet.update();
+}
 
 function sound(src) {
     this.sound = document.createElement("audio");
@@ -233,33 +224,33 @@ function sound(src) {
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function() {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function() {
         this.sound.pause();
     }    
 }
 
-function Loop1(){
-    
-        
-    if (myGamePiece.x < position.x){   
-              myGamePiece.x += 1
-          }else if (myGamePiece.x > position.x){
-              myGamePiece.x -= 1
-          }
-          if (myGamePiece.y < position.y){   
-              myGamePiece.y += 1
-          }else if (myGamePiece.y > position.y){
-              myGamePiece.y -= 1
-        } 
-    if (myGamePiece.x == position.x && myGamePiece.y == position.y||currentroom !== room){
-        walking = 0
-        clearInterval(int1)
-        
-
-  }
+function getDist(pos1, pos2) {
+	var y = pos2.y - pos1.y;
+	var x = pos2.x - pos1.x;
+	return Math.sqrt(x * x + y * y);
 }
-    
 
+function updateChar() {
+    var nx = vpos.x - myGamePiece.x;
+    var ny = vpos.y - myGamePiece.y;
+    var dist = Math.sqrt(nx * nx + ny * ny);
+
+    if (dist >= 5) {
+        myGamePiece.x += ((nx / dist) * 5);
+        myGamePiece.y += ((ny / dist) * 5);
+    }
+
+    if (myGamePiece.x == vpos.x && myGamePiece.y == vpos.y || currentroom !== room) {
+        walking = false;
+    } else {
+        requestAnimationFrame(updateChar);
+    }
+}
