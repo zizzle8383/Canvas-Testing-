ad824219e6498d3624a6d78fee05e4a7b7cf222a
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-09-21 09:55:05 +1200

    Update test.js

--- test.js
+++ test.js
@@ -167,8 +167,8 @@ function updateGameArea() {
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-       "x": evt.clientX - rect.left,
-       "y": evt.clientY - rect.top
+       "x": evt.clientX - canvas.offsetLeft,
+       "y": evt.clientY - canvas.offsetTop
     };
 }
 
