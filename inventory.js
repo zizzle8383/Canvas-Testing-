19b397efe0fe367f79c9189af27ac3c0bc3c823e
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-10-14 09:32:59 +1300

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,21 +1,10 @@
 inventory = function(){
- var self = {
-         items:[]   
-     }
-     self.additem = function(itemid){
-       self.items.push(itemid)
-      return;
-     }
-     self.hasitem = function(itemid){
-      if (self.items.includes(itemid) = true){
-            console.log("removing"+itemid+"")
-            self.items.splice(inv.items.indexOf(itemid))
-            console.log("Completed.")
-            return;
-        } else{
-         console.log("Couldnt Complete");
-         return;
-     }
-  return self;
- }
+  var self = {
+   items:[];
+  }
+  additem = function(itemid){
+   if (self.items.includes(itemid) !== false){
+   self.items.push(itemid)
+    }
+  }
 }
