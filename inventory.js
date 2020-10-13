77855e79ca20f3c37f40daaaf7ec29fcd94101a0
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-10-14 09:23:28 +1300

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -4,12 +4,17 @@ inventory = function(){
      }
      self.additem = function(itemid){
        self.items.push(itemid)
+      return;
      }
      self.hasitem = function(itemid){
       if (self.items.includes(itemid) = true){
             console.log("removing"+itemid+"")
             self.items.splice(inv.items.indexOf(itemid))
             console.log("Completed.")
-        }
+            return;
+        } else{
+         console.log("Couldnt Complete");
+         return;
      }
+  return self;
 }
