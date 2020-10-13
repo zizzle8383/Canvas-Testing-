b2f4afa5085212524358ce5b3e81ff32f2eee354
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-10-14 09:44:01 +1300

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -3,10 +3,18 @@ inventory = function(){
    items:[]
   }
   additem = function(itemid){
-   if (self.items.includes(itemid) !== false){
+   if (self.items.includes(itemid) = true){
    self.items.push(itemid)
    return;
     }
+  }
+  savedata = function(){
+    window.localStorage.addItem("Inventory",JSON.stringify(items));
+    return;
+  }
+  loaddata = function(){
+   items = JSON.parse(window.localStorage.getItem("Inventory")
+   return;
   }
 return;
 }
