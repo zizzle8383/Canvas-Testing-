140555353d75214ba136859fc27024b8f2cd73b9
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-11-03 11:25:59 +1300

    Update Market.js

--- Market.js
+++ Market.js
@@ -1,4 +1,17 @@
-var cost;
-var id;
-var num
 
+
+function buy(cost,itemid){
+  if (localstorage.getItem("cash") !== null){
+    if (localstorage.getItem("cash") >= cost){
+      localstorage.setItem("buyitem",itemid)
+      console.log("Bought"+itemid+"!")
+      
+    }else{
+      console.log("Not Enough Cash!")
+    }
+  }else{
+    localstorage.setItem("cash",0);
+    console.log("Added Cash to LocalStorage.")
+  }
+  
+  
