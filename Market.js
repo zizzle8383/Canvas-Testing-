3367b100ec2e2ed236179ac3baa125fa36ee9fee
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-11-03 11:03:57 +1300

    Update Market.js

--- Market.js
+++ Market.js
@@ -2,7 +2,7 @@ var cost;
 var id;
 var num
 function buy(ButtonClicked){
- console.log(ButtonClicked).ParentNode
+ console.log(ButtonClicked.Parent)
  parnt = ButtonClicked.ParentNode
  id = parnt.querySelector("#id").value
  cost = parnt.querySelector("#cost").value;
