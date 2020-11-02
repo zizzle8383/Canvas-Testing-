d026d2d5b644348c290a48f590a0092094880afd
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-11-03 10:38:50 +1300

    e

---
+++ Market.js
@@ -0,0 +1,11 @@
+var cost;
+var id;
+var num
+function buy(ButtonClicked){
+ parnt = ButtonClicked.Parent
+ id = parnt.querySelector("#id").value
+ cost = parnt.querySelector("#cost").value;
+ console.log(id)
+ console.log(cost)
+
+}
