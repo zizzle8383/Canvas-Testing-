24335a82447bf4a12bf110b9375871582402b376
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2020-11-20 14:10:28 +1300

    Update test.js

--- test.js
+++ test.js
@@ -53,7 +53,7 @@ function startGame() {
     myMusic.play();
     myForeground = new component(850, 50, `./rooms/${room}/fg.png`, 0, 445, "image");
     myMsg = new component("10px", "Courier New", "black", 280, 40, "text");
-    myGamePiece = new component(47.71, 47.71, ".player/llama_front.png", 425, 240, "image");
+    myGamePiece = new component(47.71, 47.71, "player/llama_front.png", 425, 240, "image");
     myNextArea =new component(50, 480, "./bordertest.png", 800, 0, "image");
     myPrevArea =new component(50, 480, "./bordertest.png", 0, 0, "image");
     myBackground = new component(850, 480, `rooms/${room}/bg.png`, 0, 0, "image");
